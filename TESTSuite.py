84d0272e927ebe4dfb243a7d163163416214a641
sania <sislam1@tulane.edu>
2024-09-23 17:05:27 -0500

    9.23 5p

--- TESTSuite.py
+++ TESTSuite.py
@@ -1,7 +1,33 @@
-#This is the test suite for SaniaAndEllasCode
+#this is our test suite
 import SaniaAndEllasCode
 import Sudoku_Examples
-if (SaniaAndEllasCode.solveSudoku(Sudoku_Examples.puzzle1, 0, 0)):
-    SaniaAndEllasCode.printing(Sudoku_Examples.puzzle1)
-else:
-    print("no solution exists ")
+def testing(grid):
+    if (SaniaAndEllasCode.solveSudoku(grid, 0, 0)):
+        SaniaAndEllasCode.printing(grid)
+    else:
+        print('no solution exists.')
+
+#works
+testing(Sudoku_Examples.puzzle)
+print('----')
+testing(Sudoku_Examples.puzzle1)
+print('----')
+testing(Sudoku_Examples.puzzle2)
+print('----')
+testing(Sudoku_Examples.puzzle3)
+print('----')
+
+print('----')
+
+print('----')
+#Prints Wrong
+testing(Sudoku_Examples.puzzle6)
+
+print('----')
+testing(Sudoku_Examples.puzzle7)
+
+#works (prints no solution)
+print('----')
+testing(Sudoku_Examples.puzzle8)
+
+#puzzle 4 and 5 overwhelm it
--- TEST_Suite_SaniaAndEllasCode.py
+++
@@ -1,7 +0,0 @@
-#This is the test suite for SaniaAndEllasCode
-import SaniaAndEllasCode
-import Sudoku_Examples
-if (SaniaAndEllasCode.solveSudoku(Sudoku_Examples.puzzle1, 0, 0)):
-    SaniaAndEllasCode.printing(Sudoku_Examples.puzzle1)
-else:
-    print("no solution exists ")
