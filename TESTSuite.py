ee1c3a503600ab101e15cc6069b4c7ee48001954
sania <sislam1@tulane.edu>
2024-12-11 15:26:02 -0600

    commit

--- TESTSuite.py
+++ TESTSuite.py
@@ -1,12 +1,28 @@
 #this is our test suite
-import SaniaAndEllasCode
+import LOGIC_16SudokuSolver
 import Sudoku_Examples
+def isValidSudoku():
+    grid = []
+    invalid_cells = []  # List to track invalid entries
+
+    # Check if the grid is valid
+    for row in range(16):
+        for col in range(16):
+            num = grid[row][col]
+            if num != 0:  # Only check non-empty cells
+                grid[row][col] = 0  # Temporarily remove the number
+                if not LOGIC_16SudokuSolver.isSafe(grid, row, col, num):
+                    invalid_cells.append((row, col))
+                grid[row][col] = num  # Restore the number
+
+    return invalid_cells
+
 def testing(grid):
-    if not SaniaAndEllasCode.isValidSudoku(grid):
+    if not isValidSudoku(grid):
         print('The given Sudoku has invalid numbers.')
     else: 
-        if (SaniaAndEllasCode.solveSudoku(grid, 0, 0)):
-            SaniaAndEllasCode.printing(grid)
+        if (LOGIC_16SudokuSolver.solveSudoku(grid, 0, 0)):
+            LOGIC_16SudokuSolver.printing(grid)
         else:
             print('no solution exists.')
 
